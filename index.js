df47c11072b17d33efb85ee90da154c2b9852c11
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-19 15:31:37 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+let divide = function divide(num1, num2){
+
+    num1 / num2;
+
+    return (2000/100)
+}
+
+let square =(num1) => (num1*num1) 
+square(5);
+
+let add = (num2,num3) => (num2 + num3)
+add(5+5);
+
